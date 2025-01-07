6804b6f60e90e2864b55eb9efc7dc37c89776285
rnignon <rnignon@naver.com>
2025-01-08 00:23:17 +0900

    init: Initial commit

---
+++ src/main/kotlin/com/example/chekitoki/ChekitokiApplication.kt
@@ -0,0 +1,11 @@
+package com.example.chekitoki
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ChekitokiApplication
+
+fun main(args: Array<String>) {
+    runApplication<ChekitokiApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/chekitoki/ChekitokiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.chekitoki
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ChekitokiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
