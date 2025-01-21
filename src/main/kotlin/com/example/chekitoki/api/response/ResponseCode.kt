8c1f5987b61cda2151f6895485ecd76590ee2b23
rnignon <rnignon@naver.com>
2025-01-21 23:05:34 +0900

    feat: 애플리케이션 내 ResponseCode 정의

---
+++ src/main/kotlin/com/example/chekitoki/api/response/ResponseCode.kt
@@ -0,0 +1,15 @@
+package com.example.chekitoki.api.response
+
+enum class ResponseCode(
+    val code: Int,
+    val message: String,
+) {
+    SUCCESS(200, "success"),
+    BAD_REQUEST(400, "bad request"),
+    FORBIDDEN(403, "forbidden"),
+    NOT_FOUND(404, "not found"),
+    METHOD_NOT_SUPPORTED(405, "method not supported"),
+    DUPLICATE(409, "duplicate"),
+    SERVER_ERROR(500, "server error"),
+    NOT_IMPLEMENTED(501, "not implemented"),
+}
