cb67bc09169772f6cfc2302d1bb1da9edd3646ef
rnignon <rnignon@naver.com>
2025-02-06 21:58:17 +0900

    feat: Unauthorized Response Code 추가

--- src/main/kotlin/com/example/chekitoki/api/response/ResponseCode.kt
+++ src/main/kotlin/com/example/chekitoki/api/response/ResponseCode.kt
@@ -6,6 +6,7 @@ enum class ResponseCode(
 ) {
     SUCCESS(200, "success"),
     BAD_REQUEST(400, "bad request"),
+    UNAUTHORIZED(401, "unauthorized"),
     FORBIDDEN(403, "forbidden"),
     NOT_FOUND(404, "not found"),
     METHOD_NOT_SUPPORTED(405, "method not supported"),
