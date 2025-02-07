fca45c0dfd241d2a689d56fdb4d733805cdb8351
rnignon <rnignon@naver.com>
2025-02-07 18:42:03 +0900

    fix: 공통 에러 응답 내 code 필드가 String으로 표기되던 사항 수정

--- src/main/kotlin/com/example/chekitoki/api/response/ApiResponse.kt
+++ src/main/kotlin/com/example/chekitoki/api/response/ApiResponse.kt
@@ -3,13 +3,13 @@ package com.example.chekitoki.api.response
 import org.springframework.validation.BindingResult
 
 class ApiResponse<T>(
-    val code: ResponseCode,
+    val code: Int,
     val message: String,
     val data: T?,
 ) {
     companion object {
         fun exceptionError(code: ResponseCode, message: String): ApiResponse<*> {
-            return ApiResponse(code, message, null)
+            return ApiResponse(code.code, message, null)
         }
 
         fun fieldError(bindingResult: BindingResult): ApiResponse<HashMap<String, String>> {
@@ -19,7 +19,7 @@ class ApiResponse<T>(
                 errors[error.field] = error.defaultMessage ?: ""
             }
 
-            return ApiResponse(ResponseCode.BAD_REQUEST, "Invalid request", errors)
+            return ApiResponse(ResponseCode.BAD_REQUEST.code, "Invalid request", errors)
         }
     }
 }
