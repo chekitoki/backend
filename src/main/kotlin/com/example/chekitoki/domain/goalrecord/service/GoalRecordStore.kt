d737297564acd5b989039f85f97218396f7848be
rnignon <rnignon@naver.com>
2025-02-15 00:19:43 +0900

    feat: GoalRecordRepository 접근 중복 제거 및 서비스 간 양방향 의존 예방을 위한 GoaRecordStore 계층 추가

---
+++ src/main/kotlin/com/example/chekitoki/domain/goalrecord/service/GoalRecordStore.kt
@@ -0,0 +1,14 @@
+package com.example.chekitoki.domain.goalrecord.service
+
+import com.example.chekitoki.domain.goal.model.Goal
+import com.example.chekitoki.domain.goalrecord.model.GoalRecord
+import java.time.LocalDate
+
+interface GoalRecordStore {
+    fun save(goalRecord: GoalRecord): GoalRecord
+    fun findOrCreate(goal: Goal, date: LocalDate?): GoalRecord
+    fun findAllByGoal(goal: Goal): List<GoalRecord>
+    fun findByGoalAndDate(goal: Goal, date: LocalDate): GoalRecord?
+    fun getById(id: Long): GoalRecord
+    fun delete(goalRecord: GoalRecord)
+}
