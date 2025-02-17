4f66b1e26d93828bc2c1ee8360a771f2cfd09c6a
rnignon <rnignon@naver.com>
2025-02-18 00:31:47 +0900

    feat: 수정 요청값 추가 및 사용하지 않는 값 삭제

--- src/test/kotlin/com/example/chekitoki/domain/fixtures/GoalRecordFixtures.kt
+++ src/test/kotlin/com/example/chekitoki/domain/fixtures/GoalRecordFixtures.kt
@@ -6,30 +6,18 @@ import com.example.chekitoki.domain.goalrecord.model.GoalRecord
 import java.time.LocalDate
 
 class GoalRecordFixtures {
-    companion object {
+     companion object {
         const val dailyGoalRecordId = 1L
         private val goalToday: LocalDate = LocalDate.now()
         private val goalYesterday: LocalDate = LocalDate.now().minusDays(1)
-        private val goalLastWeek: LocalDate = LocalDate.now().minusWeeks(1)
-        private val goalLastMonth: LocalDate = LocalDate.now().minusMonths(1)
         private const val modifiedAchievement = 1000
 
-        private val dailyGoal = GoalFixtures.dailyGoal
-        private val weeklyGoal = GoalFixtures.weeklyGoal
-        private val monthlyGoal = GoalFixtures.monthlyGoal
+        val dailyGoalRecord = GoalRecord(GoalFixtures.dailyGoal, goalToday)
+        private val dailyGoalRecordYesterday = GoalRecord(GoalFixtures.dailyGoal, goalYesterday)
 
-        val dailyGoalRecord = GoalRecord(dailyGoal, goalToday)
-        private val dailyGoalRecordYesterday = GoalRecord(dailyGoal, goalYesterday)
-        private val weeklyGoalRecord = GoalRecord(weeklyGoal, goalLastWeek)
-        private val weeklyGoalRecordLastWeek = GoalRecord(weeklyGoal, goalLastWeek)
-        private val monthlyGoalRecord = GoalRecord(monthlyGoal, goalLastMonth)
-        private val monthlyGoalRecordLastMonth = GoalRecord(monthlyGoal, goalLastMonth)
-
-        val modifiedDailyGoalRecord = GoalRecord(dailyGoal, goalToday).apply { achievement = modifiedAchievement }
+        val modifiedDailyGoalRecord = GoalRecord(GoalFixtures.dailyGoal, goalToday).apply { achievement = modifiedAchievement }
 
         val dailyGoalRecords = listOf<GoalRecord>(dailyGoalRecord, dailyGoalRecordYesterday)
-        val weeklyGoalRecords = listOf<GoalRecord>(weeklyGoalRecord, weeklyGoalRecordLastWeek)
-        val monthlyGoalRecords = listOf<GoalRecord>(monthlyGoalRecord, monthlyGoalRecordLastMonth)
 
         /* request dto */
         val createGoalRecordRequest = GoalRecordRequestDto.Create(GoalFixtures.dailyGoalId, goalToday)
@@ -39,8 +27,7 @@ class GoalRecordFixtures {
         val createGoalRecordInfo = createGoalRecordRequest.toInfo()
         val updateGoalRecordInfo = updateGoalRecordRequest.toInfo()
         val dailyGoalRecordResponseInfo = GoalRecordInfo.Response(dailyGoalRecord)
-
-        /* response */
-        val dailyGoalRecordResponseDetail = dailyGoalRecordResponseInfo.toResponseDto()
+        val dailyGoalRecordsResponseInfo = dailyGoalRecords.map { GoalRecordInfo.Response(it) }
+        val modifiedGoalRecordResponseInfo = GoalRecordInfo.Response(modifiedDailyGoalRecord)
     }
 }
