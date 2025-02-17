a50faab83d865eac80a3c26d882787e4d75cf3e7
rnignon <rnignon@naver.com>
2025-02-17 23:57:04 +0900

    fix: Long 타입 id의 조건을 NotBlank -> NotNull로 변경하여 오류 해결

--- src/main/kotlin/com/example/chekitoki/domain/goal/dto/GoalRequestDto.kt
+++ src/main/kotlin/com/example/chekitoki/domain/goal/dto/GoalRequestDto.kt
@@ -9,7 +9,7 @@ import java.time.LocalDate
 class GoalRequestDto {
     // TODO: !!를 사용하지 않고, NotBlank를 처리할 수 있는 방안 모색
     companion object {
-        const val ID_NOT_BLANK_MESSAGE = "ID를 입력해주세요."
+        const val ID_NOT_NULL_MESSAGE = "ID를 입력해주세요."
         const val TITLE_NOT_BLANK_MESSAGE = "목표명을 입력해주세요."
         const val TITLE_LENGTH_MESSAGE = "목표명은 20자 이내로 입력해주세요."
         const val DESCRIPTION_LENGTH_MESSAGE = "설명은 100자 이내로 입력해주세요."
@@ -53,7 +53,7 @@ class GoalRequestDto {
     }
 
     data class Update(
-        @field:NotBlank(message = ID_NOT_BLANK_MESSAGE)
+        @field:NotNull(message = ID_NOT_NULL_MESSAGE)
         val id: Long,
         @field:NotBlank(message = TITLE_NOT_BLANK_MESSAGE)
         @field:Length(max = 20, message = TITLE_LENGTH_MESSAGE)
